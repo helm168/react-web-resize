ce9130ce2415071dd80a00182b2a5576fa742b69
sunhao.helm <sunhao.helm@bytedance.com>
2018-04-28 21:42:07 +0800

    feat: upgrade to support react@16.x
    Change-Id: I3a7a2d4efda3bfb2bddb782062d735b154a06c33

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -10,15 +10,13 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactDom = require('react-dom');
+var _propTypes = require('prop-types');
 
-var _reactDom2 = _interopRequireDefault(_reactDom);
-
-var _proptypes = require('proptypes');
-
-var _proptypes2 = _interopRequireDefault(_proptypes);
+var _propTypes2 = _interopRequireDefault(_propTypes);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -26,113 +24,123 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Component = _react2.default.Component;
+var styles = {
+  wrapper: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    zIndex: -1
+  },
+
+  resizeWrapper: {
+    overflow: 'hidden',
+    width: '100%',
+    height: '100%'
+  },
+
+  shrink: {
+    height: '200%',
+    width: '200%'
+  }
+};
 
 var Resize = function (_Component) {
-    _inherits(Resize, _Component);
+  _inherits(Resize, _Component);
 
-    function Resize(props) {
-        _classCallCheck(this, Resize);
+  function Resize(props) {
+    _classCallCheck(this, Resize);
 
-        var _this = _possibleConstructorReturn(this, (Resize.__proto__ || Object.getPrototypeOf(Resize)).call(this, props));
+    var _this = _possibleConstructorReturn(this, (Resize.__proto__ || Object.getPrototypeOf(Resize)).call(this, props));
 
-        _this.state = {
-            domWidth: 0,
-            domHeight: 0
-        };
-        return _this;
+    _this.state = {
+      domWidth: 0,
+      domHeight: 0
+    };
+    return _this;
+  }
+
+  _createClass(Resize, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      this._resize();
     }
+  }, {
+    key: 'componentDidUpdate',
+    value: function componentDidUpdate() {
+      this.refExpand.scrollTop = this.refExpand.scrollHeight;
+      this.refExpand.scrollLeft = this.refExpand.scrollWidth;
+      this.refShrink.scrollTop = this.refShrink.scrollHeight;
+      this.refShrink.scrollLeft = this.refShrink.scrollWidth;
+    }
+  }, {
+    key: '_onExpand',
+    value: function _onExpand() {
+      this._resize();
+      if (this.props.onExpand) {
+        this.props.onExpand();
+      }
+    }
+  }, {
+    key: '_onShrink',
+    value: function _onShrink() {
+      this._resize();
+      if (this.props.onShrink) {
+        this.props.onShrink();
+      }
+    }
+  }, {
+    key: '_resize',
+    value: function _resize() {
+      var dom = this.refNode;
+      var domWidth = dom.clientWidth;
+      var domHeight = dom.clientHeight;
+      this.setState({
+        domWidth: domWidth + 1,
+        domHeight: domHeight + 1
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var _this2 = this,
+          _React$createElement;
 
-    _createClass(Resize, [{
-        key: '_onExpand',
-        value: function _onExpand() {
-            this._resize();
-            if (this.props.onExpand) {
-                this.props.onExpand();
-            }
-        }
-    }, {
-        key: '_onShrink',
-        value: function _onShrink() {
-            this._resize();
-            if (this.props.onShrink) {
-                this.props.onShrink();
-            }
-        }
-    }, {
-        key: '_resize',
-        value: function _resize() {
-            var dom = _reactDom2.default.findDOMNode(this);
-            var domWidth = dom.clientWidth;
-            var domHeight = dom.clientHeight;
-            this.setState({
-                domWidth: domWidth + 1,
-                domHeight: domHeight + 1
-            });
-        }
-    }, {
-        key: 'componentDidMount',
-        value: function componentDidMount() {
-            this._resize();
-        }
-    }, {
-        key: 'componentDidUpdate',
-        value: function componentDidUpdate() {
-            var expand = _reactDom2.default.findDOMNode(this.refs.expand);
-            expand.scrollTop = expand.scrollHeight;
-            expand.scrollLeft = expand.scrollWidth;
-            var shrink = _reactDom2.default.findDOMNode(this.refs.shrink);
-            shrink.scrollTop = shrink.scrollHeight;
-            shrink.scrollLeft = shrink.scrollWidth;
-        }
-    }, {
-        key: 'render',
-        value: function render() {
-            return _react2.default.createElement(
-                'div',
-                { style: styles.wrapper },
-                _react2.default.createElement(
-                    'div',
-                    { ref: 'expand', style: styles.resizeWrapper, onScroll: this._onExpand.bind(this) },
-                    _react2.default.createElement('div', { style: { width: this.state.domWidth, height: this.state.domHeight } })
-                ),
-                _react2.default.createElement(
-                    'div',
-                    { ref: 'shrink', style: styles.resizeWrapper, onScroll: this._onShrink.bind(this) },
-                    _react2.default.createElement('div', { style: styles.shrink })
-                )
-            );
-        }
-    }]);
+      var _state = this.state,
+          domWidth = _state.domWidth,
+          domHeight = _state.domHeight;
 
-    return Resize;
-}(Component);
+      return _react2.default.createElement(
+        'div',
+        { style: styles.wrapper, ref: function ref(node) {
+            _this2.refNode = node;
+          } },
+        _react2.default.createElement(
+          'div',
+          { style: styles.resizeWrapper,
+            ref: function ref(node) {
+              _this2.refExpand = node;
+            },
+            onScroll: this._onExpand.bind(this) },
+          _react2.default.createElement('div', { style: { width: domWidth, height: domHeight } })
+        ),
+        _react2.default.createElement(
+          'div',
+          (_React$createElement = { ref: 'shrink', style: styles.resizeWrapper
+          }, _defineProperty(_React$createElement, 'ref', function ref(node) {
+            _this2.refShrink = node;
+          }), _defineProperty(_React$createElement, 'onScroll', this._onShrink.bind(this)), _React$createElement),
+          _react2.default.createElement('div', { style: styles.shrink })
+        )
+      );
+    }
+  }]);
+
+  return Resize;
+}(_react.Component);
 
 Resize.propTypes = {
-    onExpand: _proptypes2.default.func,
-    onShrink: _proptypes2.default.func
+  onExpand: _propTypes2.default.func,
+  onShrink: _propTypes2.default.func
 };
-
-
-var styles = {
-    wrapper: {
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        zIndex: -1
-    },
-
-    resizeWrapper: {
-        overflow: 'hidden',
-        width: '100%',
-        height: '100%'
-    },
-
-    shrink: {
-        height: '200%',
-        width: '200%'
-    }
-};
-
 exports.default = Resize;
 module.exports = exports['default'];
--- index.js
+++ index.js
@@ -1,96 +1,94 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PropTypes from 'proptypes';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-let {
-    Component,
-} = React;
+const styles = {
+  wrapper: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    zIndex: -1
+  },
+
+  resizeWrapper: {
+    overflow: 'hidden',
+    width: '100%',
+    height: '100%'
+  },
+
+  shrink: {
+    height: '200%',
+    width: '200%'
+  }
+};
 
 class Resize extends Component {
-    static propTypes = {
-        onExpand: PropTypes.func,
-        onShrink: PropTypes.func,
-    }
+  static propTypes = {
+    onExpand: PropTypes.func,
+    onShrink: PropTypes.func
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            domWidth: 0,
-            domHeight: 0,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      domWidth: 0,
+      domHeight: 0
+    };
+  }
 
-    _onExpand() {
-        this._resize();
-        if(this.props.onExpand) {
-            this.props.onExpand();
-        }
-    }
+  componentDidMount() {
+    this._resize();
+  }
 
-    _onShrink() {
-        this._resize();
-        if(this.props.onShrink) {
-            this.props.onShrink();
-        }
-    }
+  componentDidUpdate() {
+    this.refExpand.scrollTop = this.refExpand.scrollHeight;
+    this.refExpand.scrollLeft = this.refExpand.scrollWidth;
+    this.refShrink.scrollTop = this.refShrink.scrollHeight;
+    this.refShrink.scrollLeft = this.refShrink.scrollWidth;
+  }
 
-    _resize() {
-        var dom = ReactDOM.findDOMNode(this);
-        var domWidth = dom.clientWidth;
-        var domHeight = dom.clientHeight;
-        this.setState({
-            domWidth: domWidth + 1,
-            domHeight: domHeight + 1
-        });
+  _onExpand() {
+    this._resize();
+    if (this.props.onExpand) {
+      this.props.onExpand();
     }
+  }
 
-    componentDidMount() {
-        this._resize();
+  _onShrink() {
+    this._resize();
+    if (this.props.onShrink) {
+      this.props.onShrink();
     }
+  }
 
-    componentDidUpdate() {
-        var expand = ReactDOM.findDOMNode(this.refs.expand);
-        expand.scrollTop = expand.scrollHeight;
-        expand.scrollLeft = expand.scrollWidth;
-        var shrink = ReactDOM.findDOMNode(this.refs.shrink);
-        shrink.scrollTop = shrink.scrollHeight;
-        shrink.scrollLeft = shrink.scrollWidth;
-    }
+  _resize() {
+    const dom = this.refNode;
+    const domWidth = dom.clientWidth;
+    const domHeight = dom.clientHeight;
+    this.setState({
+      domWidth: domWidth + 1,
+      domHeight: domHeight + 1
+    });
+  }
 
-    render() {
-        return (
-            <div style={styles.wrapper}>
-                <div ref="expand" style={styles.resizeWrapper} onScroll={this._onExpand.bind(this)}>
-                    <div style={{width: this.state.domWidth, height: this.state.domHeight}}>
-                    </div>
-                </div>
-                <div ref="shrink" style={styles.resizeWrapper} onScroll={this._onShrink.bind(this)}>
-                    <div style={styles.shrink}>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const { domWidth, domHeight } = this.state;
+    return (
+      <div style={styles.wrapper} ref={(node) => { this.refNode = node; }}>
+        <div style={styles.resizeWrapper}
+          ref={(node) => { this.refExpand = node; }}
+          onScroll={this._onExpand.bind(this)}>
+          <div style={{ width: domWidth, height: domHeight }}>
+          </div>
+        </div>
+        <div ref="shrink" style={styles.resizeWrapper}
+          ref={(node) => { this.refShrink = node; }}
+          onScroll={this._onShrink.bind(this)}>
+          <div style={styles.shrink}>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-let styles = {
-    wrapper: {
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        zIndex: -1,
-    },
-
-    resizeWrapper: {
-        overflow: 'hidden',
-        width: '100%',
-        height: '100%',
-    },
-
-    shrink: {
-        height: '200%',
-        width: '200%',
-    }
-};
 
 export default Resize;
